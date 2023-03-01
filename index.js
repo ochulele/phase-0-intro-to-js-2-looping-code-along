daa3d826d1f08a1a0c68d76fca8bc1f1a76e0aa7
ochuk <ochuciano@gmail.com>
2023-03-01 03:29:04 -0500

    looping

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(array, string){
+    const newArray = []
+    for(let i = 0 ; i < array.length; i++){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return newArray;
+}
+
+writeCards(cards);
+
+
+function countDown(number){
+    while (number >= 0){
+    console.log(number--)
+    }
+}
+
+countDown(number);
